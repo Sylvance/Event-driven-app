d9e67bec481f0ad93448e7c75b17eef1dac933da
Sylvance Mbaka <kerandisylvance@gmail.com>
2021-02-11 02:12:01 +0300

    Improve racecar.yml

--- app/consumers/user_deleted_consumer.rb
+++ app/consumers/user_deleted_consumer.rb
@@ -2,6 +2,7 @@ class UserDeletedConsumer < Racecar::Consumer
   subscribes_to "user_deleted"
 
   def process(message)
-    Rails.logger.info "Topic: #-#{message.topic} Received message: #{message.value}"
+    Rails.logger.info "Topic: #-#{message.topic}/#{message.partition} at offset #{message.offset}"
+    Rails.logger.info "Received message: #{message.value}"
   end
 end
--- app/consumers/user_viewed_consumer.rb
+++ app/consumers/user_viewed_consumer.rb
@@ -2,6 +2,7 @@ class UserViewedConsumer < Racecar::Consumer
   subscribes_to "user_viewed"
 
   def process(message)
-    Rails.logger.info "Topic: #-#{message.topic} Received message: #{message.value}"
+    Rails.logger.info "Topic: #-#{message.topic}/#{message.partition} at offset #{message.offset}"
+    Rails.logger.info "Received message: #{message.value}"
   end
 end
